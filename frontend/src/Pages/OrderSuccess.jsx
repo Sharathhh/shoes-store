5b60161d6a39be2fee5ff361cc90f4beff5545b3
Sharath Upendran <86950883+Sharathhh@users.noreply.github.com>
2025-03-27 23:01:21 +0530

    Update OrderSuccess.jsx

--- frontend/src/Pages/OrderSuccess.jsx
+++ frontend/src/Pages/OrderSuccess.jsx
@@ -4,9 +4,7 @@ import Cart from './Cart'
 import '../Pages/CSS/OrderSuccess.css'
 
 function OrderSuccess() {
- 
   const {navigate}=useNavigate()
-
   return (
     <div class='d-flex justify-content-center align-items-center flex-column min-vh-100'>
     <h1 class='text-center p-5'>Order Success</h1>
